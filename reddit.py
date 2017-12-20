396a5ed3f61a9d5d8e29a5ab24ba5503adaf1825
darthbeep <darthbeep@gmail.com>
2017-12-19 22:57:48 -0500

    cleaned up the code a little

--- reddit.py
+++ reddit.py
@@ -1,50 +1,32 @@
 import praw
+import json
 from praw.models import MoreComments, comment_forest
 def get_credentials():
     f = open("credentials", "r")
     return f.read().split('\n')
-g=get_credentials()
-print(g)
-reddit = praw.Reddit(user_agent=g[0],
-                     client_id=g[1], client_secret=g[2],
-                     username=g[3], password=g[4])
 
-subreddit = reddit.subreddit(display_name='funny')
+def get_reddit():
+    g=get_credentials()
+    return praw.Reddit(user_agent=g[0], client_id=g[1], client_secret=g[2], username=g[3], password=g[4])
 
-questions = ['what is', 'who is', 'what are']
-#something = reddit.multireddit('TheOnion', 'nottheonion').hot()
-something = reddit.subreddit('politics').hot()
+def get_comments(sub):
+    reddit = get_reddit()
+    subreddit = reddit.subreddit(sub).hot()
+    comments = []
+    for post in subreddit:
+        thread = reddit.submission(id=post)
+        for comment in thread.comments:
+            if isinstance(comment, MoreComments):
+                continue
+            comments.append(comment.body)
+    return comments
 
-print (something)
-ret = ""
-for thing in something:
-    thread = reddit.submission(id=thing)
-    #ret+= thing.title + '\n'
-    for top in thread.comments:
-        if isinstance(top, MoreComments):
-            continue
-        ret+=(top.body) + "\n"
-f = open('data2', 'w')
-f.write(ret)
-'''for submission in subreddit.stream.submissions():
-    normalized_title = submission.title.lower()
-    url = submission.url
-    print (type(submission))
-    for question_phrase in questions:
-        if question_phrase in normalized_title:
-        # do something with a matched submission
-            print ([normalized_title, url])
-            break'''
-#print (c)
-#for thing in c:
-#    print (thing)
-#for top_level_comment in submission.comments:
-#    if isinstance(top_level_comment, MoreComments):
-#        continue
-#    print(top_level_comment.body)
 
-#r=reddit#praw.Reddit('demo')
-#subreddit=r.subreddit('stackoverflow')
+def write_to_file(w):
+    print (w)
+    write = json.dumps(w)
+    f = open('data', 'w')
+    f.write(write)
 
-#for submission in subreddit.get_hot(limit=10):
-#    print (submission.title)
+c = get_comments('barnard')
+write_to_file(c)
