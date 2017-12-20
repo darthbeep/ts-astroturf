2746ee592983e65c94204d7467d3a149969f8b77
sabrina-wen <sabrinawn253@gmail.com>
2017-12-20 18:07:48 -0500

    lol

--- sentiment.py
+++ sentiment.py
@@ -14,13 +14,14 @@ with open('words.json') as json_data:
     data = json.load(json_data)
     #print(d)
 
+#clean_data = [['one', 'like', 'would', 'get', 'back'], ['would', '>', 'innocent', '|', 'death.'], ['2018', 'get', 'one', "i'm", 'election'], ['tax', 'people', 'get', 'like', "don't"], ['like', 'tax', 'people', 'republicans', 'going'], ['read', 'india', 'one', 'much', 'nepal'], ['tax', '*', 'bill', '>', '|'], ['2018', 'election', 'lottery', 'united', 'still'], ['except', 'strange', 'chicago', 'safety', 'criteria.'], ['party', 'libertarian', 'people', 'gary', 'vote'], ['*', 'people', 'like', 'would', 'government'], ['like', 'something', 'get', 'best', 'one'], ['*', '--', 'government', 'us', 'people'], ["it's", 'people', 'would', 'us', '-'], ['2018', 'election', '[primary', 'november', 'registration'], ['-', 'people', 'government', 'like', 'capitalism'], ['*', 'tax', 'people', '2018', 'would'], ['tax', 'trump', 'get', 'people', 'money'], ['us', "don't", 'would', 'like', "it's"], ['police', 'get', 'cops', 'people', 'would'], ['like', '|', "it's", 'one', 'look'], ['people', 'like', 'would', "don't", "it's"], ['tax', 'people', 'would', 'less', 'great']]
+
 clean_data = []
 
 # cleaning data: removing punctuation, uppercase, stopwords, convert to list of words
 for key in data: #list of dict values (comments)
     for element in data[key]:
-        if type(element) != int:
-            clean_data.append(element)
+        clean_data.append(element[0])
 
 print clean_data
 
@@ -34,16 +35,22 @@ def process(comment):
     data = [word for word in tokens if word not in stop] # goes through comment and removes stopwords and adds to a list
     return data
 
-for element in clean_data:
+'''for element in clean_data:
     for word in element:
-        clean_data2.append(process(word))
+        clean_data2.append(process(word))'''
+
+for element in clean_data:
+    clean_data2.append(process(word))
+
+
+#print clean_data2
 
 # convert words to vector
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 model = gensim.models.Word2Vec(clean_data2, min_count = 1)
 model.train(clean_data2, total_examples=model.corpus_count,epochs=model.iter)
-#print model
+print model
 
 #print model.most_similar(['columbia'])
 
@@ -57,6 +64,7 @@ def tsne_plot(model):
     for word in model.wv.vocab:
         tokens.append(model[word])
         labels.append(word)
+        #print word
     #print (labels)
     #print (tokens)
 
